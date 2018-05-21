4c1ffe61822d29d3a0124a81e707f2f52a74a111
Mahsa Dilmaghani <mahsadil@icloud.com>
2018-05-21 18:45:28 +0100

    small fix

--- thankyou.php
+++ thankyou.php
@@ -10,7 +10,7 @@
       <h1>Welcome <?php echo $_POST["firstname"]; ?>!</h1><br>
       <h2>Thank you for registering with us.</h2>
       <h2>We sent you a confirmation message to your</h2>
-      <h2>email address: <?php echo $_POST["email"]; ?></h2>
+      <h2>email address at <?php echo $_POST["email"]; ?></h2>
     </div>
   </div>
 </body>
